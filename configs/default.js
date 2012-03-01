e764485f19929c9e00b0725601593064287d174f
Tim Caswell <tim@creationix.com>
2012-02-29 21:55:47 -0600

    Got most of plugins working again after container refactor

--- configs/default.js
+++ configs/default.js
@@ -16,32 +16,32 @@ module.exports = {
       // This process is run as whoever owns the www folder
       uid: staticStat.uid,
       gid: staticStat.gid,
-      plugins: [
-        { module: "plugins/http.js",
+      plugins: {
+        http: { module: "./plugins/http.js",
           provides: ["http"],
           port: process.env.PORT || 80,
         },
-        { module: "plugins/static-file.js",
+        static: { module: "./plugins/static-file.js",
           dependencies: ["http"],
           root: staticDir,
         },
-        { module: "plugins/calculator.js",
+        calculator: { module: "./plugins/calculator.js",
           dependencies: ["http", "auth"],
         },
-      ],
+      },
     },
     db: {
       title: "architect-database-worker",
       uid: "nobody",
-      plugins: [
-        { module: "plugins/db.js",
+      plugins: {
+        db: { module: "./plugins/db.js",
           provides: ["database"],
         },
-        { module: "plugins/auth.js",
+        auth: { module: "./plugins/auth.js",
           dependencies: ["database"],
           provides: ["auth"],
         },
-      ]
+      }
     }
   },  
 };  
