d0c197f44c7689069eeb48e40f30b98bca6d3d08
Tim Caswell <tim@creationix.com>
2012-03-01 15:47:15 -0600

    Make the simple example simpler

--- demo/configs/simple.js
+++ demo/configs/simple.js
@@ -1,17 +1,11 @@
-var path = require('path');
-var base = path.resolve(__dirname , "..");
-var staticDir = path.join(base, "www");
-
 module.exports = {
-    name: "Architect Demo Simple",
     containers: {
         master: {
-            title: "architect-demo-simple",
             plugins: [
                 { packagePath: "architect-http",
-                  port: process.env.PORT || (process.getuid() ? 8080 : 80) },
+                  port: 8080 },
                 { packagePath: "architect-http-static",
-                  root: staticDir },
+                  root: "www" },
                 { packagePath: "../plugins/calculator" },
                 { packagePath: "../plugins/db" },
                 { packagePath: "../plugins/auth" }
