c8e4a43eedf77c8db62b7987606df0d5f211ac5c
Tim Caswell <tim@creationix.com>
2012-02-29 20:45:01 -0600

    Implement the beginnings of containers

--- configs/default.js
+++ configs/default.js
@@ -1,45 +1,47 @@
-var staticDir = __dirname + "/../www";
-var staticStat = require('fs').statSync(staticDir);
+var path = require('path');
+var fs = require('fs');
+var base = path.resolve(__dirname , "..");
+var staticDir = path.join(base, "www");
+var staticStat = fs.statSync(staticDir);
 
 module.exports = {
-	name: "Architect Demo",
-	title: "architect-demo",
-	containers: {
-		db: {
-			ssh: { host: "creationix.com" },
-			title: "architect-database-worker",
-			uid: "nobody",
-			gid: "nobody"
-		},
-		www: {
-			title: "architect-http-worker",
-			// This process is run as whoever owns the www folder
-			uid: staticStat.uid,
-			gid: staticStat.gid
-		},
-	},	
-	plugins: [
-		{	module: "../plugins/db.js",
-			provides: ["database"],
-			container: "db",
-		},
-		{	module: "../plugins/auth.js",
-			dependencies: ["database"],
-			provides: ["auth"],
-		},
-		{	module: "../plugins/http.js",
-			provides: ["http"],
-			port: process.env.PORT || 8080,
-			container: "www",
-		},
-		{	module: "../plugins/static-file.js",
-			dependencies: ["http"],
-			root: staticDir,
-			container: "www",
-		},
-		{	module: "../plugins/calculator.js",
-			dependencies: ["http", "auth"],
-			container: "www",
-		},
-	]
-};	
+  name: "Architect Demo",
+  base: base,
+  containers: {
+    master: {
+      title: "architect-demo",
+    },
+    www: {
+      title: "architect-http-worker",
+      // This process is run as whoever owns the www folder
+      uid: staticStat.uid,
+      gid: staticStat.gid,
+      plugins: [
+        { module: "plugins/http.js",
+          provides: ["http"],
+          port: process.env.PORT || 80,
+        },
+        { module: "plugins/static-file.js",
+          dependencies: ["http"],
+          root: staticDir,
+        },
+        { module: "plugins/calculator.js",
+          dependencies: ["http", "auth"],
+        },
+      ],
+    },
+    db: {
+      title: "architect-database-worker",
+      uid: "nobody",
+      plugins: [
+        { module: "plugins/db.js",
+          provides: ["database"],
+        },
+        { module: "plugins/auth.js",
+          dependencies: ["database"],
+          provides: ["auth"],
+        },
+      ]
+    }
+  },  
+};  
