dca2abefd1ff6c9b2443eb821a4e4bc01b3585db
Tim Caswell <tim@creationix.com>
2012-09-20 19:19:47 -0400

    Clean up browser example

--- demos/browser/config.js
+++ demos/browser/config.js
@@ -1,11 +1,9 @@
 require(["../../architect"], function (architect) {
-    console.log("architect", architect);
     architect.resolveConfig([
         "plugins/math",
         "plugins/app"
     ], function (err, config) {
         if (err) throw err;
-        console.log("config", config);
         architect.createApp(config);
     });
 });
--- demos/browser/plugins/app.js
+++ demos/browser/plugins/app.js
@@ -5,6 +5,9 @@ define(function () {
 
     function appPlugin(options, imports, register) {
         console.log("math", imports.math);
+        var p = document.createElement("p");
+        p.textContent = "1 + 4 = " + imports.math.add(1, 4);
+        document.body.appendChild(p);
         register();
     }
 
