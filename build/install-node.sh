16960ef39ce7129e48e7822224426e715f009afc
Matthijs van Henten <matthijs+foo@c9.io>
2018-03-08 10:56:32 +0000

    trying to get node 8 to work on codebuild

--- build/install-node.sh
+++ build/install-node.sh
@@ -1,11 +1,11 @@
-# curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 nvm install 8
 nvm alias default 8
 nvm use default
-echo which node
-echo node -v
-echo cat ~/.bashrc
+which node
+node -v
+cat ~/.bashrc
 
